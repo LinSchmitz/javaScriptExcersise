5eddae7b33f6ffd8cc5d690144ddcc4e8175ae70
naleilan <leilanschmitz@gmail.com>
2026-02-08 11:23:14 +0330

    added logic

--- Guess-The-Number/script.js
+++ Guess-The-Number/script.js
@@ -20,12 +20,22 @@ document.querySelector('.check').addEventListener('click', function () {
   } else if (secretNumber === guess) {
     document.querySelector('.message').textContent = '✔️ Correct Number';
   } else if (secretNumber > guess) {
-    document.querySelector('.message').textContent = '🔻 Too Low';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = '🔻 Too Low';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   } else {
-    document.querySelector('.message').textContent = '🔺 Too High';
-    score--;
-    document.querySelector('.score').textContent = score;
+    if (score > 1) {
+      document.querySelector('.message').textContent = '🔺 Too High';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You lost the game';
+      document.querySelector('.score').textContent = 0;
+    }
   }
 });
