4ad968c7bb12fa6b787fa1b71e55c395557552d0
naleilan <leilanschmitz@gmail.com>
2026-02-08 11:06:29 +0330

    added guess the number game

--- Guess-The-Number/script.js
+++ Guess-The-Number/script.js
@@ -4,7 +4,7 @@
 // const highScore = document.querySelector('.highscore').textContent;
 
 const secretNumber = (document.querySelector('.number').textContent =
-  Math.trunc(Math.random() * 20 + 1));
+  Math.trunc(Math.random() * 20) + 1);
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -12,23 +12,21 @@ document.querySelector('.check').addEventListener('click', function () {
   console.log(guess);
   console.log('secret number is ' + secretNumber);
 
-  if (!guess) {
+  if (!guess || guess < 0) {
     const message = (document.querySelector('.message').textContent =
       '❌ No Number');
     console.log(message);
+  } else if (secretNumber === guess) {
+    console.log('equal');
+  } else if (secretNumber > guess) {
+    console.log('Too Low');
+  } else {
+    console.log('Too High');
   }
 
-  secretNumber === guess
-    ? console.log('equal')
-    : secretNumber > guess
-      ? console.log('Too Low')
-      : console.log('Too High');
-
-  //   if (secretNumber === guess) {
-  //     console.log('equal');
-  //   } else if (secretNumber > guess) {
-  //     console.log('Too Low');
-  //   } else {
-  //     console.log('Too High');
-  //   }
+  //   secretNumber === guess
+  //     ? console.log('equal')
+  //     : secretNumber > guess
+  //       ? console.log('Too Low')
+  //       : console.log('Too High');
 });
