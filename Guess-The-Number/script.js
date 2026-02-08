2e8116ffb379c5035486ae9bb96f741b81056ae6
naleilan <leilanschmitz@gmail.com>
2026-02-08 11:17:59 +0330

    added logic

--- Guess-The-Number/script.js
+++ Guess-The-Number/script.js
@@ -1,7 +1,8 @@
 'use strict';
 
-// const score = document.querySelector('.score').textContent;
+// const score = (document.querySelector('.score').textContent = 20);
 // const highScore = document.querySelector('.highscore').textContent;
+let score = 20;
 
 const secretNumber = (document.querySelector('.number').textContent =
   Math.trunc(Math.random() * 20) + 1);
@@ -9,8 +10,8 @@ const secretNumber = (document.querySelector('.number').textContent =
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
-  console.log(guess);
-  console.log('secret number is ' + secretNumber);
+  //   console.log(guess);
+  //   console.log('secret number is ' + secretNumber);
 
   if (!guess || guess < 0) {
     const message = (document.querySelector('.message').textContent =
@@ -20,13 +21,11 @@ document.querySelector('.check').addEventListener('click', function () {
     document.querySelector('.message').textContent = '✔️ Correct Number';
   } else if (secretNumber > guess) {
     document.querySelector('.message').textContent = '🔻 Too Low';
+    score--;
+    document.querySelector('.score').textContent = score;
   } else {
     document.querySelector('.message').textContent = '🔺 Too High';
+    score--;
+    document.querySelector('.score').textContent = score;
   }
-
-  //   secretNumber === guess
-  //     ? console.log('equal')
-  //     : secretNumber > guess
-  //       ? console.log('Too Low')
-  //       : console.log('Too High');
 });
