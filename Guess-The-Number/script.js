e3e1844ca5de46b0a0cefcfa1848178026262a59
naleilan <leilanschmitz@gmail.com>
2026-02-08 10:31:01 +0330

    added guess the number game

--- Guess-The-Number/script.js
+++ Guess-The-Number/script.js
@@ -1,1 +1,10 @@
 'use strict';
+
+const message = (document.querySelector('.message').textContent =
+  'correct Number ⭐');
+const guessNumber = (document.querySelector('.guess').value = 23);
+const score = document.querySelector('.score').textContent;
+const highScore = document.querySelector('.highscore').textContent;
+const result = document.querySelector('.number').textContent;
+
+console.log(message, guessNumber, score, highScore);
