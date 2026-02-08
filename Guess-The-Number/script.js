f96f22b61bc0da3048073f31510c4a15b5e66d04
naleilan <leilanschmitz@gmail.com>
2026-02-08 13:18:20 +0330

    added logic

--- Guess-The-Number/script.js
+++ Guess-The-Number/script.js
@@ -45,11 +45,13 @@ document.querySelector('.check').addEventListener('click', function () {
 });
 
 document.querySelector('.again').addEventListener('click', function () {
+  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  score = 20;
   document.querySelector('.message').textContent = 'Start guessing...';
+  document.querySelector('.score').textContent = score;
   document.querySelector('.number').textContent = '?';
-  document.querySelector('.score').textContent = 20;
   document.querySelector('.highscore').textContent = 0;
+  document.querySelector('.guess').value = '';
   document.querySelector('.number').style.width = '15rem';
   body.style.backgroundColor = '#434141';
-  document.querySelector('.guess').value = null;
 });
