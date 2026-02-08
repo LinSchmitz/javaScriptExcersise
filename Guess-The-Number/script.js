a0604a95b9598fca7498d8941ec34603435c0124
naleilan <leilanschmitz@gmail.com>
2026-02-08 11:02:24 +0330

    added guess the number game

--- Guess-The-Number/script.js
+++ Guess-The-Number/script.js
@@ -1,22 +1,34 @@
 'use strict';
 
-// const message = (document.querySelector('.message').textContent =
-//   'correct Number ⭐');
-// const guess = (document.querySelector('.guess').value = 23);
 // const score = document.querySelector('.score').textContent;
 // const highScore = document.querySelector('.highscore').textContent;
-// const result = document.querySelector('.number').textContent;
 
-// console.log(message, guess, score, highScore);
+const secretNumber = (document.querySelector('.number').textContent =
+  Math.trunc(Math.random() * 20 + 1));
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
 
   console.log(guess);
+  console.log('secret number is ' + secretNumber);
 
   if (!guess) {
     const message = (document.querySelector('.message').textContent =
       '❌ No Number');
     console.log(message);
   }
+
+  secretNumber === guess
+    ? console.log('equal')
+    : secretNumber > guess
+      ? console.log('Too Low')
+      : console.log('Too High');
+
+  //   if (secretNumber === guess) {
+  //     console.log('equal');
+  //   } else if (secretNumber > guess) {
+  //     console.log('Too Low');
+  //   } else {
+  //     console.log('Too High');
+  //   }
 });
