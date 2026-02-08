2346859616f738fd5282f8b6dd185bae541e4dfa
naleilan <leilanschmitz@gmail.com>
2026-02-08 10:44:12 +0330

    added guess the number game

--- Guess-The-Number/script.js
+++ Guess-The-Number/script.js
@@ -1,10 +1,22 @@
 'use strict';
 
-const message = (document.querySelector('.message').textContent =
-  'correct Number ⭐');
-const guessNumber = (document.querySelector('.guess').value = 23);
-const score = document.querySelector('.score').textContent;
-const highScore = document.querySelector('.highscore').textContent;
-const result = document.querySelector('.number').textContent;
+// const message = (document.querySelector('.message').textContent =
+//   'correct Number ⭐');
+// const guess = (document.querySelector('.guess').value = 23);
+// const score = document.querySelector('.score').textContent;
+// const highScore = document.querySelector('.highscore').textContent;
+// const result = document.querySelector('.number').textContent;
 
-console.log(message, guessNumber, score, highScore);
+// console.log(message, guess, score, highScore);
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  console.log(guess);
+
+  if (!guess) {
+    const message = (document.querySelector('.message').textContent =
+      '❌ No Number');
+    console.log(message);
+  }
+});
