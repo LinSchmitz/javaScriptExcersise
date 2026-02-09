f465a0445d49f8c0c128a4f4d283292d824dc266
naleilan <leilanschmitz@gmail.com>
2026-02-09 10:57:32 +0330

    added some logic

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -10,32 +10,47 @@ const player2 = document.querySelector('.player--1');
 const player1TotalScore = document.querySelector('#score--0');
 const player1CurrentScore = document.querySelector('#current--0');
 
+const player2TotalScore = document.querySelector('#score--1');
+const player2CurrentScore = document.querySelector('#current--1');
+
+const diceImage = document.querySelector('.dice');
+
 // 👉 REAL game data (state)
-let currentScore = 0;
-let totalScore = 0;
+let diceImg;
+
+let p1CurrentScore = 0;
+let p1TotalScore = 0;
+
+let p2CurrentScore = 0;
+let p2TotalScore = 0;
 
 // Set total score to 0 at the beginning
-player1TotalScore.textContent = totalScore;
+player1TotalScore.textContent = p1TotalScore;
+player2TotalScore.textContent = p2TotalScore;
+
+diceImage.classList.add('hidden');
 
 const rollDice = function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
+  console.log(dice);
 
-  currentScore = dice;
-  player1CurrentScore.textContent = currentScore;
+  p1CurrentScore = dice;
+  player1CurrentScore.textContent = p1CurrentScore;
+
+  //dice image
 };
 
+//Roll the Dice
 roll.addEventListener('click', rollDice);
 
+// hold the game
 hold.addEventListener('click', function () {
-  totalScore += currentScore;
-
+  p1TotalScore += p1CurrentScore;
   // Update DOM
-  player1TotalScore.textContent = totalScore;
-
+  player1TotalScore.textContent = p1TotalScore;
   // Reset current score
-  currentScore = 0;
+  p1CurrentScore = 0;
   player1CurrentScore.textContent = 0;
-
   //switch player
   if (player1.classList.contains('player--active')) {
     player1.classList.remove('player--active');
