035f9397708cf4217246fa2450013aa0a68a91ce
naleilan <leilanschmitz@gmail.com>
2026-02-09 09:06:29 +0330

    refactor

--- Modal/script.js
+++ Modal/script.js
@@ -5,19 +5,19 @@ const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnsShowModal = document.querySelectorAll('.show-modal');
 
-for (let i = 0; i < btnsShowModal.length; i++) {
-  btnsShowModal[i].addEventListener('click', function () {
-    console.log(`Button ${i + 1} cliked`);
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
-
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-btnCloseModal.addEventListener('click', closeModal);
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
 
+for (let i = 0; i < btnsShowModal.length; i++) {
+  btnsShowModal[i].addEventListener('click', openModal);
+}
+
+btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
