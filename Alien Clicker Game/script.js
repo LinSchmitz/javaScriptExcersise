78ef06cd22e3278d85e0ffa9a6cc2a14d20dc8b8
naleilan <leilanschmitz@gmail.com>
2026-02-09 15:55:55 +0330

    added alien game

--- Alien Clicker Game/script.js
+++ Alien Clicker Game/script.js
@@ -1,1 +1,13 @@
 'use strict';
+
+const alien = document.querySelector('.alien');
+const scoreEl = document.querySelector('#score');
+const livesEl = document.querySelector('#lives');
+
+let score = 0;
+
+alien.addEventListener('click', function () {
+  score = score + 1;
+  scoreEl.textContent = score;
+  alien.classList.add('hidden');
+});
