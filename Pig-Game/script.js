122e57b2d092555b22b1f186bdf6d82bf3c2fda1
naleilan <leilanschmitz@gmail.com>
2026-02-09 10:37:04 +0330

    added some logic

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -7,18 +7,29 @@ const newGame = document.querySelector('.btn--new');
 const player1TotalScore = document.querySelector('#score--0');
 const player1CurrentScore = document.querySelector('#current--0');
 
-console.log(player1TotalScore);
-console.log(player1CurrentScore);
+// 👉 REAL game data (state)
+let currentScore = 0;
+let totalScore = 0;
+
+// Set total score to 0 at the beginning
+player1TotalScore.textContent = totalScore;
 
 const rollDice = function () {
-  let randomNumber = Math.trunc(Math.random() * 6) + 1;
-  player1CurrentScore.textContent = randomNumber;
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  currentScore = dice;
+  player1CurrentScore.textContent = currentScore;
 };
 
 roll.addEventListener('click', rollDice);
 
 hold.addEventListener('click', function () {
-  player1TotalScore.textContent =
-    Number(player1TotalScore.textContent) +
-    Number(player1CurrentScore.textContent);
+  totalScore += currentScore;
+
+  // Update DOM
+  player1TotalScore.textContent = totalScore;
+
+  // Reset current score
+  currentScore = 0;
+  player1CurrentScore.textContent = 0;
 });
