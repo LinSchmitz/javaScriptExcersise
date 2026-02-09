a1c421031375567695a72741be4a1c05a1cacd39
naleilan <leilanschmitz@gmail.com>
2026-02-09 16:13:58 +0330

    added alien game

--- Alien Clicker Game/script.js
+++ Alien Clicker Game/script.js
@@ -4,10 +4,30 @@ const alien = document.querySelector('.alien');
 const scoreEl = document.querySelector('#score');
 const livesEl = document.querySelector('#lives');
 
+const gameW = document.querySelector('.game').clientWidth;
+const gameH = document.querySelector('.game').clientHeight;
+
+const alienW = document.querySelector('.alien').clientWidth;
+const alienH = document.querySelector('.alien').clientHeight;
+
+console.log(gameH, gameW, alienW, alienH);
+
 let score = 0;
+const maxTop = gameH - alienH;
+const maxLeft = gameW - alienW;
 
 alien.addEventListener('click', function () {
+  let randomTop = Math.trunc(Math.random() * maxTop);
+  let randomLeft = Math.trunc(Math.random() * maxLeft);
+
   score = score + 1;
   scoreEl.textContent = score;
   alien.classList.add('hidden');
+
+  alien.style.top = randomTop + 'px';
+  alien.style.left = randomLeft + 'px';
+
+  setTimeout(() => {
+    alien.classList.remove('hidden');
+  }, 1000);
 });
