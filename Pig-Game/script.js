aaba8f55f322710f695746482b960004748726c0
naleilan <leilanschmitz@gmail.com>
2026-02-09 10:20:50 +0330

    added pig game

--- Pig-Game/script.js
+++ Pig-Game/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const roll = document.querySelector('.btn--roll');
+const hold = document.querySelector('.btn--hold');
+const newGame = document.querySelector('.btn--new');
+
+const player1TotalScore = document.querySelector('#score--0');
+const player1CurrentScore = document.querySelector('#current--0');
+
+console.log(player1TotalScore);
+console.log(player1CurrentScore);
+
+const rollDice = function () {
+  let randomNumber = Math.trunc(Math.random() * 6) + 1;
+  player1CurrentScore.textContent = randomNumber;
+};
+
+roll.addEventListener('click', rollDice);
+
+hold.addEventListener('click', function () {
+  player1TotalScore.textContent =
+    Number(player1TotalScore.textContent) +
+    Number(player1CurrentScore.textContent);
+});
