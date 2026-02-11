9135016d29cb9f502f2e983e4777578bb22a7805
naleilan <leilanschmitz@gmail.com>
2026-02-11 09:50:25 +0330

    Added behind the scene

--- Behind-the-Scenes/script.js
+++ Behind-the-Scenes/script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2026 - birthYear;
+
+  function printAge() {
+    const optput = `${firstName} has ${age}, born in ${birthYear}`;
+    console.log(optput);
+  }
+  printAge();
+
+  return age;
+}
+
+const firstName = 'lil';
+calcAge(1991);
