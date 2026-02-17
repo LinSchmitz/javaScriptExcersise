e502317dc5ca3431c95b0696783570afff7702c3
naleilan <leilanschmitz@gmail.com>
2026-02-17 10:59:35 +0330

    added practice

--- Data-Structures-Operators/script.js
+++ Data-Structures-Operators/script.js
@@ -140,8 +140,16 @@ const restaurant = {
 // The for-of Loop
 const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
-for (const item of menu) console.log(item);
+// for (const item of menu) console.log(item);
 
-for (const [i, el] of menu.entries()) {
-  console.log(`${i + 1}: ${el}`);
-}
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i + 1}: ${el}`);
+// }
+
+const printName = function (...menu) {
+  for (const [i, el] of menu.entries()) {
+    console.log(`${i + 1}: ${el}`);
+  }
+};
+
+printName(...restaurant.starterMenu);
