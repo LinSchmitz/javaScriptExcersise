41c36fc3637ac3cc8fe5aeb000c2a38033e10d75
naleilan <leilanschmitz@gmail.com>
2026-02-22 09:17:50 +0330

    added map

--- Data-Structures-Operators/script.js
+++ Data-Structures-Operators/script.js
@@ -299,36 +299,65 @@ const ordersSet = new Set([
 
 // console.log(italianFoods.isDisjointFrom(mexicanFoods));
 
+//1️⃣ Comparing Data (VERY common in real apps)
+const oldTags = ['js', 'react', 'css'];
+const newTags = ['js', 'react', 'node'];
+
+const added = new Set(newTags).difference(new Set(oldTags));
+const removed = new Set(oldTags).difference(new Set(newTags));
+
+console.log(added, removed);
+
+//2️⃣ Permissions / Roles Systems
+const adminPermissions = new Set(['read', 'write', 'delete']);
+const userPermissions = new Set(['read']);
+
+console.log(adminPermissions.intersection(userPermissions));
+
+//3️⃣ Avoiding Duplicate Processing (VERY common)
+const processedUsers = new Set();
+
+//4️⃣ Data Sync / Caching Problems
+//Frontend receives new data from API and must compare with cached data.
+
+cachedIds.union(newIds);
+cachedIds.difference(newIds);
+
 //////////////////////////////////////////////
 //////////////////////////////////////////////
 //////////////////////////////////////////////
-// Maps: Fundamentals
-const rest = new Map();
+// // Maps: Fundamentals
+// const rest = new Map();
 
-rest.set('name', 'Classico Italiano');
-rest.set(1, 'Firenze, Italy');
-console.log(rest.set(2, 'Lisbon, Portugal'));
+// rest.set('name', 'Classico Italiano');
+// rest.set(1, 'Firenze, Italy');
+// console.log(rest.set(2, 'Lisbon, Portugal'));
 
-rest
-  .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
-  .set('open', 11)
-  .set('close', 23)
-  .set(true, 'We are open :D')
-  .set(false, 'We are closed :(');
+// rest
+//   .set('categories', ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'])
+//   .set('open', 11)
+//   .set('close', 23)
+//   .set(true, 'We are open :D')
+//   .set(false, 'We are closed :(');
 
-console.log(rest.get('name'));
-console.log(rest.get(true));
-console.log(rest.get(1));
+// console.log(rest.get('name'));
+// console.log(rest.get(true));
+// console.log(rest.get(1));
 
-const time = 8;
-console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
+// const time = 8;
+// console.log(rest.get(time > rest.get('open') && time < rest.get('close')));
 
 // console.log(rest.has('categories'));
 // rest.delete(2);
-// // rest.clear();
+// rest.clear();
+
+// rest.set([1, 2], 'test');
+// console.log(rest.get[(1, 2)]); //undefined , why?
+// //they are not the same object in the heap
 
 // const arr = [1, 2];
-// rest.set(arr, 'Test');
+// console.log(rest.set(arr, 'Test')); // its working now
+
 // rest.set(document.querySelector('h1'), 'Heading');
 // console.log(rest);
 // console.log(rest.size);
