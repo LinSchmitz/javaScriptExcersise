c2a970b6ee5018d3b7fdddccbdf391adaf64e77d
naleilan <leilanschmitz@gmail.com>
2026-02-26 09:33:25 +0330

    added pollApp

--- pollApp/script.js
+++ pollApp/script.js
@@ -1,39 +1,35 @@
 'use strict';
 
-// Test data
-const data1 = [5, 2, 3];
-const data2 = [1, 5, 3, 9, 6, 1];
-
-const results1 = { answers: data1 };
-const results2 = { answers: data2 };
-
 const poll = {
   question: 'What is your favourite programming language?',
-  options: ['0: JavaScript', '1: Python', '2: Rust', '3:C++'],
+  options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+
+  // [0, 0, 0, 0]
   answers: new Array(4).fill(0),
 
+  // 1. Register answer
   registerNewAnswer() {
     const inputNumber = Number(
-      prompt(`What is your favourite programming language?\n
-        0: JavaScript
-        1: Python
-        2: Rust
-        3: C++`),
+      prompt(
+        `${this.question}\n${this.options.join('\n')}\n(Write option number)`,
+      ),
     );
 
+    // Validate input
     if (
       Number.isInteger(inputNumber) &&
       inputNumber >= 0 &&
-      inputNumber <= this.options.length - 1
+      inputNumber < this.answers.length
     ) {
       this.answers[inputNumber]++;
     }
 
-    // 📢 show results
+    // 4. Display results after answering
     this.displayResults();
     this.displayResults('string');
   },
 
+  // 3. Display results
   displayResults(type = 'array') {
     if (type === 'array') {
       console.log(this.answers);
@@ -43,17 +39,22 @@ const poll = {
   },
 };
 
+// 2. Call when user clicks button
 document
   .querySelector('.poll')
   .addEventListener('click', poll.registerNewAnswer.bind(poll));
 
-// Bind displayResults to those objects
-const showResults1 = poll.displayResults.bind(results1);
-const showResults2 = poll.displayResults.bind(results2);
+/* ======================
+   BONUS (Different Data)
+   ====================== */
 
-// Call with both options
-showResults1('array');
-showResults1('string');
+// Test data (NOT inside poll)
+const data1 = [5, 2, 3];
+const data2 = [1, 5, 3, 9, 6, 1];
 
-showResults2('array');
-showResults2('string');
+// Fake objects so "this.answers" works
+poll.displayResults.call({ answers: data1 }, 'array');
+poll.displayResults.call({ answers: data1 }, 'string');
+
+poll.displayResults.call({ answers: data2 }, 'array');
+poll.displayResults.call({ answers: data2 }, 'string');
