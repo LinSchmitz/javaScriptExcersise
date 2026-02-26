5a3e59743ee6bc256d37b6583e61c4c6ad02ad0f
naleilan <leilanschmitz@gmail.com>
2026-02-26 09:31:41 +0330

    added pollApp

--- pollApp/script.js
+++ pollApp/script.js
@@ -1,61 +1,59 @@
 'use strict';
 
+// Test data
+const data1 = [5, 2, 3];
+const data2 = [1, 5, 3, 9, 6, 1];
+
+const results1 = { answers: data1 };
+const results2 = { answers: data2 };
+
 const poll = {
   question: 'What is your favourite programming language?',
   options: ['0: JavaScript', '1: Python', '2: Rust', '3:C++'],
-  // This generates [0, 0, 0, 0]. More in the next section!
   answers: new Array(4).fill(0),
+
   registerNewAnswer() {
     const inputNumber = Number(
       prompt(`What is your favourite programming language?\n
         0: JavaScript
         1: Python
         2: Rust
-        3: C++
-        `),
+        3: C++`),
     );
-    console.log(inputNumber);
+
     if (
       Number.isInteger(inputNumber) &&
       inputNumber >= 0 &&
       inputNumber <= this.options.length - 1
-    )
+    ) {
       this.answers[inputNumber]++;
-    console.log(this.answers);
+    }
+
+    // 📢 show results
+    this.displayResults();
+    this.displayResults('string');
+  },
+
+  displayResults(type = 'array') {
+    if (type === 'array') {
+      console.log(this.answers);
+    } else if (type === 'string') {
+      console.log(`Poll results are ${this.answers.join(', ')}`);
+    }
   },
 };
 
-// 1. Create a method called 'registerNewAnswer' on the 'poll' object. The
-//    method does 2 things:
-//    1.1. Display a prompt window for the user to input the number of the
-//    selected option. The prompt should look like this:
-//    What is your favourite programming language?
-//    0: JavaScript
-//    1: Python
-//    2: Rust
-//    3: C++
-//    (Write option number)
-//    1.2. Based on the input number, update the 'answers' array property. For
-//    example, if the option is 3, increase the value at position 3 of the array by
+document
+  .querySelector('.poll')
+  .addEventListener('click', poll.registerNewAnswer.bind(poll));
 
-// const registerNewAnswer = function () {
-//   const inputNumber = Number(
-//     prompt(`What is your favourite programming language?\n
-//         0: JavaScript
-//         1: Python
-//         2: Rust
-//         3: C++
-//         `),
-//   );
-//   console.log(inputNumber);
-//   if (
-//     Number.isInteger(inputNumber) &&
-//     inputNumber >= 0 &&
-//     inputNumber <= poll.options.length - 1
-//   )
-//     poll.answers[inputNumber]++;
-//   console.log(poll.answers);
-// };
-// registerNewAnswer();
-//  Data 1: [5, 2, 3]
-//   Data 2: [1, 5, 3, 9, 6, 1]
+// Bind displayResults to those objects
+const showResults1 = poll.displayResults.bind(results1);
+const showResults2 = poll.displayResults.bind(results2);
+
+// Call with both options
+showResults1('array');
+showResults1('string');
+
+showResults2('array');
+showResults2('string');
