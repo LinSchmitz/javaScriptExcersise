ce8ceab2fac045b0770b4a69ce036db9804a649d
naleilan <leilanschmitz@gmail.com>
2026-02-27 10:38:25 +0330

    added pollApp

--- ClosureExercise/script-final.js
+++ ClosureExercise/script-final.js
@@ -64,16 +64,17 @@
 
 ///////////////////////////////////////
 // Coding Challenge #2
+// Take the IIFE below and at the end of the function,
+// attach an event listener that changes the color of the
+// selected h1 element ('header') to blue, each time the BODY
+//  element is clicked. Do NOT select the h1 element again!
 
-/* 
-This is more of a thinking challenge than a coding challenge 🤓
+// And now explain to YOURSELF (or someone around you) WHY this
+//  worked! Take all the time you need. Think about WHEN exactly
+//   the callback function is executed, and what that means for the
+//   variables involved in this example.
 
-Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
-
-And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
-
-GOOD LUCK 😀
-*/
+// GOOD LUCK 😀
 
 /*
 (function () {
