3e5b2001dda8298ec1356bc99d6605010c0fd231
naleilan <leilanschmitz@gmail.com>
2026-06-02 09:13:17 +0400

    constructor function

--- OOP/script.js
+++ OOP/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const Person = function (firstName, birthDate) {
+  //Instance properties
+  this.firstName = firstName;
+  this.birthDate = birthDate;
+
+  //bad practice
+  //   this.calcAge = function () {
+  //     console.log(2037 - this.birthDate);
+  //   };
+};
+
+const jonas = new Person('jonas', 1991);
+console.log(jonas);
