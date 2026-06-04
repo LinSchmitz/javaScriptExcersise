23c7e82ec28cb1ccba193ec22299e3b34694956e
naleilan <leilanschmitz@gmail.com>
2026-06-04 09:06:07 +0400

    request data

--- CountryListAPI/script.js
+++ CountryListAPI/script.js
@@ -12,10 +12,26 @@ const countriesContainer = document.querySelector('.countries');
 ///////////////////////////////////////
 
 const request = new XMLHttpRequest();
-request.open('GET', 'https://restcountries.com/v3.1/name/iran');
+request.open('GET', 'https://restcountries.com/v3.1/name/germany');
 request.send();
 
 request.addEventListener('load', function () {
   const [data] = JSON.parse(this.responseText);
   console.log(data);
+
+  const html = `
+         <article class="country ">
+          <img class="country__img" src="${data.flags.png}" />
+          <div class="country__data">
+            <h3 class="country__name">${data.name.common}</h3>
+            <h4 class="country__region">${data.region}</h4>
+            <p class="country__row"><span>👫</span>${(+data.population / 1000000).toFixed(1)}</p>
+            <p class="country__row"><span>🗣️</span>${Object.values(data.languages)[0]}</p>
+            <p class="country__row"><span>💰</span>${Object.values(data.currencies)[0].name}</p>
+          </div>
+        </article>
+  `;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
 });
